8ab411172bc33739e49b446818681165e9797e01
谢佳君 <1003742236@qq.com>
2019-01-23 15:09:02 +0800

    修改了忽略词的实现方法

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 ## 指定想搜索的豆瓣电影以及想爬取的最大评论数与前TOP的排名
 KEYWORD = '地球最后的夜晚'
-MAX_COUNT = 20
+MAX_COUNT = 220
 TOP_NUM = 10
 
 ## 指定想要忽略的词
-IGNORE = [",", "，", ".", "。", " ", "#", "！"]
+IGNORE = ["电影"]
 
 ## 指定python绘制词云的字体路径、图片路径以及保存路径
 FONT_PATH = 'resources/SimHei.ttf'
